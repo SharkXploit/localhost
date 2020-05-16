72ae93d6f88c3be9b48284f7a042a77aa635e7e0
SharkXploit <46089648+SharkXploit@users.noreply.github.com>
2020-05-16 23:51:41 +0700

    Update local.sh

--- local.sh
+++ local.sh
@@ -2,7 +2,7 @@
 clear
 figlet LocalHost
 echo "SharkXploiter Crew"
-echo "╭─Masukkan Port yang akan di gunakan di localhost example (8888)
+echo "╭─Masukkan Port yang akan di gunakan di localhost example (8888)"
 read -p "╰─>" port
 echo "╭─Masukkan File yang akan di jalankan di localhost : "
 read -p "╰─>" file

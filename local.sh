f9536233e5405a8b82109f4358546993edc151cd
SharkXploit <46089648+SharkXploit@users.noreply.github.com>
2020-05-16 23:49:12 +0700

    Create local.sh

---
+++ local.sh
@@ -0,0 +1,9 @@
+#!/bin/bash                                                           
+clear
+figlet LocalHost
+echo "SharkXploiter Crew"
+echo "╭─Masukkan Port yang akan di gunakan di localhost example (8888)
+read -p "╰─>" port
+echo "╭─Masukkan File yang akan di jalankan di localhost : "
+read -p "╰─>" file
+php -S 127.0.0.1:$port $file
